4fa8149acad0d8fda308ed3083ca4334aa3231c4
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-18 17:23:59 +0530

    basic

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
-
 const app = express();
-app.get("",(req,resp)=>{
-    resp.send("<h1>Basic node js example</h1>")
-});
-app.get("/about",(req,resp)=>{
-    resp.send("<h1>This is About Page </h1>")
-});
-app.get("/contact",(req,resp)=>{
-    resp.send("<h1>This is contact Page </h1>")
-});
 
-app.listen(3200)
+// const express = require('express')();
+
+app.get("",(req,resp)=>{
+    resp.send("<h1>Home Page</h1>")
+})
+
+
+app.get("/about",(req,resp)=>{
+    resp.send("<h1>About Page</h1>")
+})
+
+
+app.listen(3400)
+
+
+
