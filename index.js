f1631cbfafd8e68de1db420ccc093c6f939638ee
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-19 12:16:19 +0530

    done

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const express = require('express');
+// const express = require('express');
+
+import express from 'express';
+import home, { contact } from './pages/home.js';
+import about from './pages/about.js';
+
 const app = express();
 
-// const express = require('express')();
-
 app.get("",(req,resp)=>{
-    resp.send("<h1>Home Page</h1>")
+    resp.send(home())
 })
 
 
 app.get("/about",(req,resp)=>{
-    resp.send("<h1>About Page</h1>")
+    resp.send(about())
 })
+
+app.get("/contact",(req,resp)=>{
+    resp.send(contact())
+})
+
 
 
 app.listen(3400)
