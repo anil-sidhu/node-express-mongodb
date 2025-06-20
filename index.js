00d28fb018b5dc6b254345d8444efe5704554a7d
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-20 16:59:46 +0530

    commit

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-// const express = require('express');
-
-import express from 'express';
-import home, { contact } from './pages/home.js';
-import about from './pages/about.js';
+import express from 'express'
+import home from './pages/home.js';
+import login from './pages/login.js';
+import submit from './pages/submit.js';
 
 const app = express();
 
-app.get("",(req,resp)=>{
-    resp.send(home())
+app.get("/",(req,resp)=>{
+    resp.send(home());
+})
+app.get("/login",(req,resp)=>{
+    resp.send(login());
+})
+app.post("/submit",(req,resp)=>{
+    resp.send(submit());
 })
 
-
-app.get("/about",(req,resp)=>{
-    resp.send(about())
-})
-
-app.get("/contact",(req,resp)=>{
-    resp.send(contact())
-})
-
-
-
-app.listen(3400)
-
-
-
+app.listen(3200)
--- pages/about.js
+++
@@ -1,3 +0,0 @@
-export default function about(){
-    return "<h1>About Page with ES export way</h1>"
-}
