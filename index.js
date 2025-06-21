dbd6e68e1992662ba07fc628f5cee12d7251ddf1
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-22 01:17:19 +0530

    worked on 404 page

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 import express from 'express'
 import path from 'path'
 const app = express();
-
+ const absPath= path.resolve('view')
 app.get("/",(res,resp)=>{
-    const absPath= path.resolve('view/home.html')
-    resp.sendFile(absPath)
+   
+    resp.sendFile(absPath+"/home.html")
 })
 app.get("/login",(res,resp)=>{
-    const absPath= path.resolve('view/login.html')
-    resp.sendFile(absPath)
+
+    resp.sendFile(absPath+"/login.html")
 })
 
 app.get("/about",(res,resp)=>{
-    const absPath= path.resolve('view/about.html')
-    resp.sendFile(absPath)
+
+    resp.sendFile(absPath+"/about.html")
 })
 
+app.use((req,resp)=>{
+   
+    resp.status(404).sendFile(absPath+"/404.html")
+})
 
 app.listen(3200)
