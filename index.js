3171575740ba87105f3bc9de7d600ea9e160cd02
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-24 02:20:44 +0530

    worked on route middleware

--- index.js
+++ index.js
@@ -2,41 +2,36 @@ import express from 'express'
 const app = express();
 
 
-// function ageCheck(req,resp,next){
-//     if(!req.query.age || req.query.age<18){
-//        resp.send("Alert ! You can not access this page")
-//     }else{
-//         next();
-//     }
-// }
-
-// app.use(ageCheck)
-
-function ipCheck(req,resp,next){
-  const ip = req.socket.remoteAddress
-  console.log(ip);
-  if(ip.includes('192.168.1.95')){
-      resp.send("Alert ! You can not access this page")
-  }else{
-next()  
-  }
-  
-  
+function checkAgeRouteMiddleware(req,resp,next){
+    console.log(req.query.age);
+    
+    if(!req.query.age || req.query.age<18 ){
+        resp.send("You are not allowed to used this website")
+    }else{
+        next()
+    }
 }
 
-app.use(ipCheck)
+function checkURLRouteMiddleware(req,resp,next){
+    console.log("this request url is ", req.url);
 
+   next()
+}
 
-app.get("/",(req,resp)=>{
-    resp.send("<h1>Home Page</h1>")
-});
+app.get('',(req,resp)=>{
+    resp.send("<h1>Home Page<h1>")
+})
 
-app.get("/login",(req,resp)=>{
-    resp.send("<h1>Login Page</h1>")
-});
+app.get('/login',checkURLRouteMiddleware,(req,resp)=>{
+    resp.send("<h1>Login Page<h1>")
+})
 
-app.get("/admin",(req,resp)=>{
-    resp.send("<h1>Admin Page</h1>")
-});
+app.get('/users',checkAgeRouteMiddleware,checkURLRouteMiddleware,(req,resp)=>{
+    resp.send("<h1>User Page<h1>")
+})
+
+app.get('/products',checkAgeRouteMiddleware,checkURLRouteMiddleware,(req,resp)=>{
+    resp.send("<h1>Products Page<h1>")
+})
 
 app.listen(3200)
