86b61e296b69e06671d92ef2a55b089d1fa852e9
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-25 15:46:42 +0530

    done

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import express from 'express'
-import morgan from 'morgan';
 const app = express();
 
-app.use(morgan('dev'))
 app.get("/",(req,resp)=>{
-    resp.send("Home page")
+resp.send("Home Page")
 });
 
 app.get("/users",(req,resp)=>{
-    resp.send("Users page")
+resp.send("Users Page")
 });
 
-app.get("/wait",(req,resp)=>{
-   setTimeout(() => {
-     resp.send("result after 1 second")
-   }, 1000);
+
+app.get("/error",(req,resp,next)=>{
+const error= new Error('')
+error.status=404;
+next(error)
+});
+
+
+
+app.use((error, req,resp,next)=>{
+   resp.status(error.status || 500).send("Try after some time")
 })
 
 app.listen(3200)
