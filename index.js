a10ca5663c70509ea7802cd5260cec9e97abd6ed
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-27 18:53:51 +0530

    done

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-import express from 'express'
-const app = express();
+// import express from 'express'
+// const app = express();
 
 
-app.use(express.urlencoded({extended:false}))
-app.set('view engine','ejs')
-app.get('/add-user',(req,resp)=>{
-    resp.render('addUser')
-});
+// app.use(express.urlencoded({extended:false}))
+// app.set('view engine','ejs')
+// app.get('/add-user',(req,resp)=>{
+//     resp.render('addUser')
+// });
 
-
-app.post('/submit-user',(req,resp)=>{
-    console.log(req.body);
+// app.post('/submit-user',(req,resp)=>{
+//     console.log(req.body);
     
-resp.render('SubmitUser',req.body)
-});
+// resp.render('SubmitUser',req.body)
+// });
 
-app.listen(3200)
+// app.get("/",(req,resp)=>{
+
+//      const fruits = ["Apple", "Banana", "Orange"];
+//   resp.render("home", { fruits,login:true });
+// })
+
+// app.listen(3200)
+
