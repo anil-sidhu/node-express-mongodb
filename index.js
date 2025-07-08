df251572c9e0f81728a0d9bb0ee0ebbe60666a82
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-07-08 18:51:48 +0530

    done

--- index.js
+++ index.js
@@ -7,21 +7,24 @@ const client= new MongoClient(url)
 
 
 const app = express();
-
 app.set("view engine",'ejs')
-app.get("/",async (req,resp)=>{
+client.connect().then((connection)=>{
+    const db =connection.db(dbName);
 
-     await client.connect()
-    const db = client.db(dbName);
-    const collection= db.collection('students')
-
-    const students = await collection.find().toArray()
-    console.log(students);
-    
-
-    resp.render('students',{students})
-
+    app.get("/api",async (req,resp)=>{
+        const collection =db.collection("students")
+        const students= await collection.find().toArray();
+        resp.send(students)
+    })
+     app.get("/ui",async (req,resp)=>{
+        const collection =db.collection("students")
+        const students= await collection.find().toArray();
+        resp.render('students',{students})
+    })
 })
+
+// app.set("view engine",'ejs')
+
 app.listen(3200)
 
 
