cae26f99fce98c98d018bc9bdb336ccc4a3b2efd
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-07-09 15:34:46 +0530

    done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const client= new MongoClient(url)
 
 
 const app = express();
+app.use(express.urlencoded({extended:true}))
 app.set("view engine",'ejs')
 client.connect().then((connection)=>{
     const db =connection.db(dbName);
@@ -21,6 +22,22 @@ client.connect().then((connection)=>{
         const students= await collection.find().toArray();
         resp.render('students',{students})
     })
+
+    app.get('/add',(req,resp)=>{
+        resp.render('add-student')
+    })
+
+     app.post("/add-student",async (req,resp)=>{
+        // console.log(req.body);
+        
+        const collection =db.collection("students")
+        const result= await collection.insertOne(req.body)
+        console.log(result);
+        
+        // const students= await collection.find().toArray();
+        resp.send("data saved")
+    })
+
 })
 
 // app.set("view engine",'ejs')
