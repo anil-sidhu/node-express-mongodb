3559007924fb2435a10fda19cb03e1bcc3acdda8
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-07-15 00:49:23 +0530

    done

--- index.js
+++ index.js
@@ -36,6 +36,38 @@ if(!req.body || !name || !age || !email){
 
 })
 
+app.put("/update/:id",async (req,resp)=>{
+    
+    const id =req.params.id;
+    
+    console.log(req.body,id);
+
+    const studentData= await studentModel.findByIdAndUpdate(id,{
+        ...req.body
+    })
+    resp.send({
+        message:'data updated',
+        success:true,
+        info:studentData
+    })
+
+})
+
+
+app.delete("/delete/:id",async (req,resp)=>{
+    
+    const id =req.params.id;
+    
+
+    const studentData= await studentModel.findByIdAndDelete(id)
+    resp.send({
+        message:'data delete',
+        success:true,
+        info:studentData
+    })
+
+})
+
 app.listen(3200)
 
 // async function dbConnection(){
