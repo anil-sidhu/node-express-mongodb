8bf47903aada4dc26995cf8c398bf5182ff10063
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-07-14 22:45:25 +0530

    yt

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import studentModel from './model/studentModel.js';
 
 const app = express();
 
-
+ app.use(express.json());
  await mongoose.connect("mongodb://localhost:27017/school").then(()=>{
     console.log("_______connected______");
  })
@@ -13,6 +13,27 @@ const app = express();
 app.get("/",async (req,resp)=>{
     const studentData= await studentModel
     resp.send(studentData)
+})
+
+app.post("/save",async (req,resp)=>{
+    console.log(req.body);
+    const {name,age,email}= req.body;
+if(!req.body || !name || !age || !email){
+  resp.send({
+        message:"data not stored",
+        success:false,
+        storedInfo:null
+    })  
+    return false
+}
+    const studentData = await studentModel.create(req.body)
+    
+    resp.send({
+        message:"data stored",
+        success:true,
+        storedInfo:studentData
+    })
+
 })
 
 app.listen(3200)
