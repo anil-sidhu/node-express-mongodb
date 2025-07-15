0700c11fccd67961079c8aab9a1f773a5b5bfd76
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-07-15 16:44:22 +0530

    done

--- index.js
+++ index.js
@@ -1,88 +1,14 @@
-import mongoose from 'mongoose'
 import express from 'express'
-import studentModel from './model/studentModel.js';
+import cors from 'cors'
+const app= express();
 
-const app = express();
-
- app.use(express.json());
- await mongoose.connect("mongodb://localhost:27017/school").then(()=>{
-    console.log("_______connected______");
- })
-
-
-app.get("/",async (req,resp)=>{
-    const studentData= await studentModel
-    resp.send(studentData)
-})
-
-app.post("/save",async (req,resp)=>{
-    console.log(req.body);
-    const {name,age,email}= req.body;
-if(!req.body || !name || !age || !email){
-  resp.send({
-        message:"data not stored",
-        success:false,
-        storedInfo:null
-    })  
-    return false
-}
-    const studentData = await studentModel.create(req.body)
-    
+app.use(cors());
+app.get("/",(req,resp)=>{
     resp.send({
-        message:"data stored",
-        success:true,
-        storedInfo:studentData
-    })
-
-})
-
-app.put("/update/:id",async (req,resp)=>{
-    
-    const id =req.params.id;
-    
-    console.log(req.body,id);
-
-    const studentData= await studentModel.findByIdAndUpdate(id,{
-        ...req.body
-    })
-    resp.send({
-        message:'data updated',
-        success:true,
-        info:studentData
-    })
-
-})
-
-
-app.delete("/delete/:id",async (req,resp)=>{
-    
-    const id =req.params.id;
-    
-
-    const studentData= await studentModel.findByIdAndDelete(id)
-    resp.send({
-        message:'data delete',
-        success:true,
-        info:studentData
-    })
-
+        name:"anil",
+        age:29,
+        email:"anil@test.com"
+    }) 
 })
 
 app.listen(3200)
-
-// async function dbConnection(){
-//    await mongoose.connect("mongodb://localhost:27017/school");
-//    const schema= mongoose.Schema({
-//     name:String,
-//     email:String,
-//     age:Number,
-//    })
-
-//    const studentsModel = mongoose.model('students',schema);
-//    const result = await studentsModel.find();
-//    console.log(result);
-   
-
-// }
-
-// dbConnection();
