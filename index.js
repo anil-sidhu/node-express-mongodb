01fcd7b5b8f2a62c4da1ee8405f2844f6543e151
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-07-24 01:32:47 +0530

    worked on add task

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 import express from 'express'
 import path from 'path'
+import { MongoClient } from 'mongodb';
 
 const app = express();
 const publicPath = path.resolve('public')
 app.use(express.static(publicPath));
-app.set("view engine",'ejs')
+app.set("view engine", 'ejs')
+
+const dbName = "node-project";
+const collectionName = "todo"
+const url = "mongodb://localhost:27017"
+const client = new MongoClient(url)
+
+const connection = async () => {
+    const connect = await client.connect();
+    return await connect.db(dbName)
+}
 
 
-
-app.get("/",(req,resp)=>{
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, resp) => {
     resp.render("list")
 })
 
-app.get("/add",(req,resp)=>{
-   resp.render("add")
+app.get("/add", (req, resp) => {
+    resp.render("add")
 })
 
-app.get("/update",(req,resp)=>{
+app.get("/update", (req, resp) => {
     resp.render("update")
 })
 
-app.post("/update",(req,resp)=>{
+app.post("/update", (req, resp) => {
     resp.redirect("/")
 })
 
-app.post("/add",(req,resp)=>{
-    resp.redirect("/")
+app.post("/add", async (req, resp) => {
+    const db = await connection();
+    const collection = db.collection(collectionName);
+    const result = collection.insertOne(req.body)
+    if (result) {
+        resp.redirect("/")
+    } else {
+        resp.redirect("/add")
+    }
+
 })
 
 
