b8e406c693f08eae8511ebd02cc873665ce59aa7
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-07-28 02:03:49 +0530

    worked

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import path from 'path'
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 const app = express();
 const publicPath = path.resolve('public')
@@ -48,6 +48,18 @@ app.post("/add", async (req, resp) => {
         resp.redirect("/")
     } else {
         resp.redirect("/add")
+    }
+
+})
+
+app.get("/delete/:id", async (req, resp) => {
+    const db = await connection();
+    const collection = db.collection(collectionName);
+    const result = collection.deleteOne({_id:new ObjectId(req.params.id)})
+    if (result) {
+        resp.redirect("/")
+    } else {
+        resp.send("/some error")
     }
 
 })
