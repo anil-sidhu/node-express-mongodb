c1326b7f9d1694ceb810db7c5a66ca3b5f0da973
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-08-28 22:43:10 +0530

    done

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node + Express on Vercel!");
+  res.send(`<h1>Apple</h1>
+    go to api link
+    <a href="/api" >API</a>
+    `);
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    "id": 1,
+    "name": "Anil Sidhu",
+    "email": "anil.sidhu@example.com",
+    "isActive": true,
+    "roles": ["admin", "editor"],
+    "profile": {
+      "age": 28,
+      "gender": "male",
+      "skills": ["JavaScript", "React", "Node.js"]
+    },
+    "createdAt": "2025-08-28T22:15:00Z"
+  }
+  );
 });
 
 app.listen(3000, () => console.log("Server running..."));
